8f1c9d7baeb6a1a5946f65ab6e1e8a540c4b097b
SovannrothEang <eangsovannroth@gmail.com>
2026-01-06 18:23:01 +0700

    Not sure with but will go as it is

--- src/modules/prisma/prisma.service.ts
+++ src/modules/prisma/prisma.service.ts
@@ -3,76 +3,106 @@ import { PrismaClient } from '@prisma/client';
 import { Pool } from 'pg';
 import { PrismaPg } from '@prisma/adapter-pg';
 
+interface SoftDeleteCapable {
+    [model: string]: {
+        delete: (args: any) => Promise<any>;
+        deleteMany: (args: any) => Promise<any>;
+        update: (args: any) => Promise<any>;
+        updateMany: (args: any) => Promise<any>;
+        findFirst: (args: any) => Promise<any>;
+        findMany: (args: any) => Promise<any>;
+    };
+}
+
 @Injectable()
 export class PrismaService
-  extends PrismaClient
-  implements OnModuleInit, OnModuleDestroy {
-  // This property will hold the logic-enriched client
-  private readonly _extendedClient;
+    extends PrismaClient
+    implements OnModuleInit, OnModuleDestroy
+{
+    // This property will hold the logic-enriched client
+    private readonly _extendedClient: unknown;
 
-  constructor() {
-    const connectionString = process.env.DATABASE_URL || '';
-    const pool = new Pool({ connectionString });
-    const adapter = new PrismaPg(pool);
-    super({ adapter });
-    this._extendedClient = this.setupSoftDelete();
-  }
+    constructor() {
+        const connectionString = process.env.DATABASE_URL || '';
+        const pool = new Pool({ connectionString });
+        const adapter = new PrismaPg(pool);
+        super({ adapter });
+        this._extendedClient = this.setupSoftDelete();
+    }
 
-  get client() {
-    return this._extendedClient;
-  }
+    get client() {
+        return this._extendedClient;
+    }
 
-  async onModuleInit(): Promise<void> {
-    await this.$connect();
-  }
+    async onModuleInit(): Promise<void> {
+        await this.$connect();
+    }
 
-  async onModuleDestroy(): Promise<void> {
-    await this.$disconnect();
-  }
+    async onModuleDestroy(): Promise<void> {
+        await this.$disconnect();
+    }
 
-  private setupSoftDelete() {
-    const softDeleteModels: string[] = [
-      'User',
-      'Employee',
-      'Department',
-      'Payroll',
-    ];
+    private setupSoftDelete() {
+        const softDeleteModels: string[] = [
+            'User',
+            'Employee',
+            'Department',
+            'Payroll',
+        ];
 
-    return this.$extends({
-      query: {
-        $allModels: {
-          async delete({ model, args }) {
-            if (softDeleteModels.includes(model)) {
-              return (this as any).update({
-                ...args,
-                data: { isDeleted: true, deletedAt: new Date() },
-              });
-            }
-            return (this as any).delete(args);
-          },
-          async deleteMany({ model, args }) {
-            if (softDeleteModels.includes(model)) {
-              return (this as any).updateMany({
-                ...args,
-                data: { isDeleted: true, deletedAt: new Date() },
-              });
-            }
-            return (this as any).deleteMany(args);
-          },
-          async findMany({ model, args, query }) {
-            if (softDeleteModels.includes(model)) {
-              args.where = { isDeleted: false, ...args.where };
-            }
-            return query(args);
-          },
-          async findFirst({ model, args, query }) {
-            if (softDeleteModels.includes(model)) {
-              args.where = { isDeleted: false, ...args.where };
-            }
-            return query(args);
-          },
-        },
-      },
-    });
-  }
+        return this.$extends({
+            query: {
+                $allModels: {
+                    async delete({ model, args }) {
+                        if (softDeleteModels.includes(model)) {
+                            // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+                            return (this as unknown as SoftDeleteCapable)[
+                                model
+                            ].update({
+                                ...args,
+                                data: {
+                                    isDeleted: true,
+                                    deletedAt: new Date(),
+                                },
+                            });
+                        }
+                        // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+                        return (this as unknown as SoftDeleteCapable)[
+                            model
+                        ].delete(args);
+                    },
+                    async deleteMany({ model, args }) {
+                        if (softDeleteModels.includes(model)) {
+                            // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+                            return (this as unknown as SoftDeleteCapable)[
+                                model
+                            ].updateMany({
+                                ...args,
+                                data: {
+                                    isDeleted: true,
+                                    deletedAt: new Date(),
+                                },
+                            });
+                        }
+                        // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+                        return (this as unknown as SoftDeleteCapable)[
+                            model
+                        ].deleteMany(args);
+                    },
+                    async findMany({ model, args, query }) {
+                        if (softDeleteModels.includes(model)) {
+                            args.where = { isDeleted: false, ...args.where };
+                        }
+                        return query(args);
+                    },
+                    async findFirst({ model, args, query }) {
+                        if (softDeleteModels.includes(model)) {
+                            args.where = { isDeleted: false, ...args.where };
+                        }
+                        return query(args);
+                    },
+                },
+            },
+        });
+    }
 }
